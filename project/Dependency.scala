e9f8d087c77e3ba7d1c9294c3fab0aea09acdbd2
Diogo Sousa <diogogsousa@gmail.com>
2019-06-01 11:33:30 +0100

    Bump depepency versions.

--- project/Dependency.scala
+++ project/Dependency.scala
@@ -3,9 +3,9 @@ import sbt._
 object Dependency {
   private object Version {
     val Cats1         = "1.6.0"
-    val CatsEffect1   = "1.3.0"
+    val CatsEffect1   = "1.3.1"
     val Scalaz7       = "7.2.27"
-    val ScalazZio1    = "1.0-RC4"
+    val ScalazZio1    = "1.0-RC5"
     val ScalaLogging3 = "3.9.2"
     val ScalaTest     = "3.0.7"
   }
